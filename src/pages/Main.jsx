25cecb466ca9c734be639de3e57a24a9034103ae
하진 <khjtwin11@gmail.com>
2026-02-04 22:51:09 +0900

    Revert "[Feat]:: 메인 페이지 header, hero, footer 구현"

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,17 +1,9 @@
 import Header from "../components/Header";
-import Hero from "../pages//Hero";
-import CoreValues from "./CoreValues";
-import QnA from "./QnA";
-import Footer from "../components/Footer";
 
 function Main() {
   return (
     <>
-      <Header />
-      <Hero />
-      <CoreValues />
-      {/* <QnA /> */}
-      <Footer />
+      <Header/>
     </>
   );
 }
