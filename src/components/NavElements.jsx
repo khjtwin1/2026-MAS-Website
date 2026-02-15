592e5e18475d2ceeca454170ee9376bd8dba2975
Hajin Kim <khjtwin11@gmail.com>
2026-02-15 23:42:18 +0900

    [Chore]:: 이전 파일 오류 수정

--- src/components/NavElements.jsx
+++ src/components/NavElements.jsx
@@ -1,21 +1,21 @@
-import { Link } from "react-router-dom";
-import "../styles/NavElements.css";
+  import { Link } from "react-router-dom";
+  import "../styles/NavElements.css";
 
-function NavElements({ text, path, isHighlight }) {
-  const className = isHighlight ? "navElements highlight" : "navElements";
-    if(path.startsWith("#")){
-      return (
-        <li>
-          <a className={className} href={path}>{text}</a>
-        </li>
-      );
-    } else {
-      return (
-        <Link className={className} to={path}>
-          {text}
-        </Link>
-      );
-    }
-}
+  function NavElements({ text, path, isHighlight }) {
+    const className = isHighlight ? "navElements highlight" : "navElements";
+      if(path.startsWith("#")){
+        return (
+          <li>
+            <a className={className} href={path}>{text}</a>
+          </li>
+        );
+      } else {
+        return (
+          <Link className={className} to={path}>
+            {text}
+          </Link>
+        );
+      }
+  }
 
-export default NavElements;
+  export default NavElements;
