cfb7bdc50feceb59bad65c7bb6a8cf72bf536444
hajungwon32 <netrock49@gmail.com>
2026-02-20 00:23:30 +0900

    [Feat]:: 프로젝트 폴더 생성

--- data/projectsList.js
+++ data/projectsList.js
@@ -1,97 +1,97 @@
 const projectsList = [
   {
-    image: "/SignLanguageTranslator.svg",
+    image: "/projects/SignLanguageTranslator.svg",
     type:'WEB',
     year:'2025',
     title:'수화번역기',
     subTitle: "비장애인과 장애인의 소통 장벽을 허물어 소통을 돕는 서비스" 
   },
   {
-    image: "/Dalab.svg",
+    image: "/projects/Dalab.svg",
     type:'APP',
     year:'2025',
     title:'다랩',
     subTitle: "칭찬과 격려를 시각화 해주는 칭찬도장 시스템" 
   },
   {
-    image: "/Ulikkili.svg",
+    image: "/projects/Ulikkili.svg",
     type:'WEB',
     year:'2025',
     title:'우리끼리',
     subTitle: "학교 생활을 더 편리하고 즐겁게 만들기 위한\n우리반만의 프라이빗한 공간" 
   },
   {
-    image: "/Oleum.svg",
+    image: "/projects/Oleum.svg",
     type:'APP',
     year:'2025',
     title:'오름',
     subTitle: "“등산”을 통해 매일 목표를 세워 할 일을 통해\n휴대폰 시간을 줄이는 디지털디톡스 서비스" 
   },
   {
-    image: "/GreetingChallenge.svg",
+    image: "/projects/GreetingChallenge.svg",
     type:'WEB',
     year:'2025',
     title:'인사챌린지',
     subTitle: "미림의 전통인 인사를 게임으로 쉽게 배울 수 있는 서비스" 
   },
   {
-    image: "/STEPBYSTEP.svg",
+    image: "/projects/STEPBYSTEP.svg",
     type:'WEB',
     year:'2025',
     title:'STEPBYSTEP',
     subTitle: "건강에 한걸을 한걸음" 
   },
   {
-    image: "/MUSEWAVE.svg",
+    image: "/projects/MUSEWAVE.svg",
     type:'APP',
     year:'2025',
     title:'MUSE WAVE',
     subTitle: "사용자 맞춤형 AI튜터가 공감대를 형성하여\n공부를 도와주는 맞춤형 AI튜터 학습 앱 입니다." 
   },
   {
-    image: "/ReadGo.svg",
+    image: "/projects/ReadGo.svg",
     type:'APP',
     year:'2025',
     title:'읽go',
     subTitle: "논문, 보고서, 전공 서적 등 어렵고 깊이 있는 글을 제대로 소화하고,\n자신의 지식으로 만들도록 돕는 AI 학습 파트너" 
   },
   {
-    image: "/SajuBoys.svg",
+    image: "/projects/SajuBoys.svg",
     type:'WEB',
     year:'2025',
     title:'사주보이즈',
     subTitle: "사주를 보다 정확하고 간편하게 해주는 사주 명리학을 기반으로 한 사주 웹사이트" 
   },
   {
-    image: "/SCHOK.svg",
+    image: "/projects/SCHOK.svg",
     type:'WEB',
     year:'2025',
     title:'SCHOK',
     subTitle: "속초시 학생들에게 꼭 필요한 정보만 모은 정보 플랫폼" 
   },
   {
-    image: "/Signal.svg",
+    image: "/projects/Signal.svg",
     type:'WEB',
     year:'2025',
     title:'SIGNAL',
     subTitle: "온라인으로 한 걸음 다가가는 미림인을 위한 서비스" 
   },
   {
-    image: "/Nyangnyangmasmap.svg",
+    image: "/projects/Nyangnyangmasmap.svg",
     type:'WEB',
     year:'2025',
     title:'냥냥마스맵',
     subTitle: "길고양이를 좋아하는 사람들을 위한 길고양이 커뮤니티"
   },
   {
-    image: "/Momento.svg",
+    image: "/projects/Momento.svg",
     type:'APP',
     year:'2025',
     title:'Momento',
     subTitle: "반 친구들이 선생님이 내준 미션 또는 과제를 완료하면,\n학급 캐릭터가 성장하여 반의 단합, 선생님의 학생관리를\n동시에 해결하는 앱입니다." 
   },
   {
-    image: "/POP!CK.svg",
+    image: "/projects/POP!CK.svg",
     type:'APP',
     year:'2025',
     title:'POP!CK',
