2501d0960fe788d60db3efcdea501bc21d9f2de9
Hajin Kim <khjtwin11@gmail.com>
2026-02-19 22:57:17 +0900

    [Chore]:: 타이핑 효과 추가

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,26 +1,34 @@
 import "../styles/Hero.css";
 import Apply from "../components/Apply";
+import Typewriter from 'typewriter-effect';
 
 function Hero() {
   const mars = "/mars.svg";
   const whiteRing = "/whiteRing.svg"
   const orangeRing = "/orangeRing.svg"
+  
   return (
     <section id="hero">
-      <div className="hero">
-        <div className="heroRow">
-          <div>
-            <h1 className="texts">
-              <p>SHARE<br/>YOUR</p>
-              <p className="orange">ABILITY</p>
-            </h1>
-            <Apply/>
+      <div className="heroRow">
+        <div className="left">
+          <div className="texts">
+            <Typewriter
+              options={{
+                strings: [
+                  'SHARE<br/>YOUR<br/><span class="orange">ABILITY</span>',
+                  'MY<br/><span class="orange">ABILITY</span><br/>SHARE'
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
           </div>
-          <div className="planet">
-            <img className="mars" src={mars} alt="행성" />
-            <img className="wR" src={whiteRing} alt="흰고리" />
-            <img className="oR" src={orangeRing} alt="주황고리" />
-          </div>
+          <Apply/>
+        </div>
+        <div className="planet">
+          <img className="mars" src={mars} alt="행성" />
+          <img className="wR" src={whiteRing} alt="흰고리" />
+          <img className="oR" src={orangeRing} alt="주황고리" />
         </div>
       </div>
     </section>
