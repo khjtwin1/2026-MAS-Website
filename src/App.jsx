a8cbcf35bfbfc2cc6cbe349b1e59b419b7e4af3f
hajungwon32 <netrock49@gmail.com>
2026-02-22 22:53:56 +0900

    [Refactor]: 불필요한 주석 제거

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
-      {/*프로젝트 페이지 중 자세히 보기를 누르면 이동하는 페이지 */}
-      <Route path="/projects" element={<Projects />} />  {/* All */}
+      <Route path="/projects" element={<Projects />} />
       
       
     </Routes>
