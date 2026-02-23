66fd3d851340813fe275edeec18ea73944b21228
Hajin Kim <khjtwin11@gmail.com>
2026-02-24 08:37:32 +0900

    [Chore]:: 전페이지 합치기 및 css 수정

--- src/pages/OurPrize.jsx
+++ src/pages/OurPrize.jsx
@@ -10,35 +10,29 @@ function OurPrize() {
 
   return (
     <section id="ourPrize">
-      <div className="ourPrizeSection">
-        <div className="planet_wrapper">
-          <img src={planetIMG} alt="" className="ourprize_background"/>
-          <div className="planet_center1">
-            <img src={miniTitanIMG} alt="" className="miniTitan1"/>
-            <img src={miniTitanIMG} alt="" className="miniTitan2"/> 
-               
-          </div>
-          <div className="planet_center2">
-            <img src={miniTitanIMG} alt="" className="miniTitan3"/> 
-          </div>
+      <div className="planet_wrapper">
+        <img src={planetIMG} alt="" className="ourprize_background"/>
+        <div className="planet_center1">
+          <img src={miniTitanIMG} alt="" className="miniTitan1"/>
+          <img src={miniTitanIMG} alt="" className="miniTitan2"/> 
         </div>
-        
-        <div className="ourPrize-left">
-          <h1 className="our">OUR</h1>
-          <h1 className="prize">PRIZE</h1>
-
-          <p className="explanation">우리는 매년 새로운 기록을 경신하며 <br />학교를 대표하는 전공 동아리로 거듭나고 있습니다.</p>
-
-          <button className="morePrize" onClick={() => {
-            navigate("/prize");
-          }}>
-            더 많은 수상내역 보기<img src={nextButtonIMG} alt="" className="nextButton"/></button>    
+        <div className="planet_center2">
+          <img src={miniTitanIMG} alt="" className="miniTitan3"/> 
         </div>
+      </div>
+      
+      <div className="ourPrize-left">
+        <h1 className="our">OUR</h1>
+        <h1 className="prize">PRIZE</h1>
+        <p className="explanation">우리는 매년 새로운 기록을 경신하며 <br />학교를 대표하는 전공 동아리로 거듭나고 있습니다.</p>
+        <button className="morePrize" onClick={() => {
+          navigate("/prize");
+        }}>
+          더 많은 수상내역 보기<img src={nextButtonIMG} alt="" className="nextButton"/></button>    
+      </div>
 
-        <div className="ourpPicture">
-          <img src={everyPictureIMG} alt="" className="ourprizePicture"/>
-        </div>  
-        
+      <div className="ourpPicture">
+        <img src={everyPictureIMG} alt="" className="ourprizePicture"/>
       </div>
     </section>
     
